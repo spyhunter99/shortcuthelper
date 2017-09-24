22fe621b751594d0aef83a5b4f55f79cf39c7d7f
Alex O'Ree <alexoree@apache.org>
2017-09-24 12:47:47 -0400

    reducing the helper class to a minimum

--- src/main/java/com/github/spyhunter99/shortcuthelper/Main.java
+++ src/main/java/com/github/spyhunter99/shortcuthelper/Main.java
@@ -6,16 +6,11 @@
 package com.github.spyhunter99.shortcuthelper;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import org.netbeans.installer.utils.FileUtils;
-import org.netbeans.installer.utils.ResourceUtils;
-import org.netbeans.installer.utils.StreamUtils;
-import org.netbeans.installer.utils.StringUtils;
 import org.netbeans.installer.utils.SystemUtils;
 import org.netbeans.installer.utils.exceptions.NativeException;
 import org.netbeans.installer.utils.system.shortcut.FileShortcut;
@@ -24,179 +19,147 @@ import org.netbeans.installer.utils.system.shortcut.Shortcut;
 
 public class Main {
 
-    public static final String SHORTCUT_FILENAME
-            = ResourceUtils.getString(Main.class, "CL.app.name") + ".desktop"; // NOI18N
-
+    //prerequistes
+    //you need an icon
+    //windows must be bitmap with the .ico file extension
     public static final String[] SHORTCUT_CATEGORIES = new String[]{
         "Application"
     };
 
-    public static final String BIN_SUBDIR
-            = "bin/";
+    /**
+     *
+     * @param desktop
+     * @param startmenu
+     * @param installLocation
+     * @param executable
+     * @param productName
+     * @param productDescription
+     * @param relativePathStartMenu
+     * @param icon windows needs to be a bitmap .bmp with the extension .ico<br>
+     * linux/unit based systems needs a TBD <br>
+     * macos based systems needs a .icns or a png/jpeg
+     * file
+     * @return
+     */
+    public static List<File> createShortCuts(boolean desktop,
+            boolean startmenu,
+            File installLocation, //path to where it runs
+            String executable, //your jar file
+            String productName,
+            String productDescription,
+            String relativePathStartMenu,
+            String icon) {
+        List<File> filesCreated = new ArrayList<File>();
+        if (desktop) {
+            //desktop shortcut
+            if (!SystemUtils.isMacOS()) {
+                try {
+                    if (SystemUtils.isCurrentUserAdmin()) {
+                        filesCreated.add(SystemUtils.createShortcut(
+                                getDesktopShortcut(installLocation, executable, productName, productDescription, icon),
+                                LocationType.ALL_USERS_DESKTOP));
 
-    public static final String EXECUTABLE_WINDOWS
-            = BIN_SUBDIR
-            + ResourceUtils.getString(Main.class, "CL.app.name") + ".exe"; // NOI18N
+                    } else {
+                        filesCreated.add(
+                                SystemUtils.createShortcut(
+                                        getDesktopShortcut(installLocation, executable, productName, productDescription, icon),
+                                        LocationType.CURRENT_USER_DESKTOP));
+                    }
+                } catch (NativeException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //"... skipping this step as we're on Mac OS"); // NOI18N
+            }
 
-    public static final String EXECUTABLE_UNIX
-            = BIN_SUBDIR
-            + ResourceUtils.getString(Main.class, "CL.app.name"); // NOI18N
+        }
 
-    public static final String ICON_WINDOWS
-            = EXECUTABLE_WINDOWS;
-
-    public static final String ICON_UNIX
-            = ResourceUtils.getString(Main.class,
-                    "CL.unix.icon.name"); // NOI18N
-
-    public static final String ICON_UNIX_RESOURCE
-            = ResourceUtils.getString(Main.class,
-                    "CL.unix.icon.resource"); // NOI18N
-
-    public static final String ICON_MACOSX
-            = ResourceUtils.getString(Main.class, "CL.app.name") + ".icns"; // NOI18N
-
-    public static final String ICON_MACOSX_RESOURCE
-            = "org/mycompany/" + ResourceUtils.getString(Main.class, "CL.app.name") + ".icns"; // NOI18N
-
-    static Map<Locale, String> productName = new HashMap<Locale, String>();
-    static Map<Locale, String> productDescription = new HashMap<Locale, String>();
-    static String relativePath = "Shortcut Helper";
-    static {
-        productName.put(Locale.getDefault(), "ShortcutHelper");
-        productDescription.put(Locale.getDefault(), "A product description");
-    }
-    
-    
-    public static void main(String[] args) throws NativeException {
-
-        File installLocation = new File("C:\\projects\\ShortCutHelper\\target\\");
-        String executable = "ShortCutHelper-8.2.0-jar-with-dependencies.jar";
-        new Main().install(installLocation, executable);
-    }
-    public void install(File installLocation, String  executable) {
-        //desktop shortcut
-        if (!SystemUtils.isMacOS()) {
+        if (startmenu) {
+            /////////////////////////////////////////////
+            // create start menu shortcut
             try {
                 if (SystemUtils.isCurrentUserAdmin()) {
-                    SystemUtils.createShortcut(
-                            getDesktopShortcut(installLocation, executable),
-                            LocationType.ALL_USERS_DESKTOP);
-
+                    filesCreated.add(
+                            SystemUtils.createShortcut(
+                                    getStartMenuShortcut(installLocation, executable, productName, productDescription, relativePathStartMenu, icon),
+                                    LocationType.ALL_USERS_START_MENU));
                 } else {
-                    SystemUtils.createShortcut(
-                            getDesktopShortcut(installLocation, executable),
-                            LocationType.CURRENT_USER_DESKTOP);
+                    filesCreated.add(
+                            SystemUtils.createShortcut(
+                                    getStartMenuShortcut(installLocation, executable, productName, productDescription, relativePathStartMenu, icon),
+                                    LocationType.CURRENT_USER_START_MENU));
                 }
             } catch (NativeException e) {
                 e.printStackTrace();
             }
-        } else {
-            //"... skipping this step as we're on Mac OS"); // NOI18N
+
         }
+        return filesCreated;
 
-        /////////////////////////////////////////////
-        // create start menu shortcut
-        try {
-            if (SystemUtils.isCurrentUserAdmin()) {
-
-                SystemUtils.createShortcut(
-                        getStartMenuShortcut(installLocation,executable),
-                        LocationType.ALL_USERS_START_MENU);
-            } else {
-                SystemUtils.createShortcut(
-                        getStartMenuShortcut(installLocation,executable),
-                        LocationType.CURRENT_USER_START_MENU);
-            }
-        } catch (NativeException e) {
-            e.printStackTrace();
-        }
-
-        //Shortcut sc = new FileShortcut("Shortcut title", new File("path/to/executable"));
-        //SystemUtils.createShortcut(sc, LocationType.CURRENT_USER_DESKTOP);
     }
 
-    private Shortcut getDesktopShortcut(final File directory, String  executable) {
-        
-        
+    public static void main(String[] args) throws NativeException {
+
+        File installLocation = new File("C:\\projects\\ShortCutHelper\\target\\");
+
+        List<File> createShortCuts = createShortCuts(true, true, installLocation, "ShortCutHelper-8.2.0-jar-with-dependencies.jar", "My Product Name", "This is a cool description", "Github", "icon");
+        for (File f : createShortCuts) {
+            System.out.println("Created " + f.getAbsolutePath());
+        }
+    }
+
+    private static Shortcut getDesktopShortcut(final File directory, String executable, String productName, String productDescription, String icon) {
+
         return getShortcut(
                 productName,
                 productDescription,
-                "",
-                directory,executable);
+                "", //put it right on the desktop
+                directory, executable, icon);
     }
 
-    private Shortcut getStartMenuShortcut(final File directory, String  executable) {
+    private static Shortcut getStartMenuShortcut(final File directory, String executable, String productName, String productDescription, String relativePathStartMenu, String icon) {
         if (SystemUtils.isMacOS()) {
-              return getShortcut(
-                productName,
-                productDescription,
-                relativePath,     //relative path within the start menu
-                directory,executable);
+            return getShortcut(
+                    productName,
+                    productDescription,
+                    relativePathStartMenu, //relative path within the start menu
+                    directory, executable, icon);
         } else {
-              return getShortcut(
-                productName,
-                productDescription,
-               relativePath,     //relative path within the start menu
-                directory,executable);
+            return getShortcut(
+                    productName,
+                    productDescription,
+                    relativePathStartMenu, //relative path within the start menu
+                    directory, executable, icon);
         }
     }
 
-    private Shortcut getShortcut(
-            final Map<Locale, String> names,
-            final Map<Locale, String> descriptions,
+    private static Shortcut getShortcut(
+            final String productName,
+            final String productDescription,
             final String relativePath,
-            final File workingDirectory, 
-            final String executableName) {
-        final File icon;
-        final File executable;
+            final File workingDirectory,
+            final String executableName,
+            final String icon) {
 
-        if (SystemUtils.isWindows()) {
-            icon = new File(workingDirectory, ICON_WINDOWS);
-        } else if (SystemUtils.isMacOS()) {
-            icon = new File(workingDirectory, ICON_MACOSX);
-        } else {
-            icon = new File(workingDirectory, ICON_UNIX);
-            //LogManager.log("... icon file: " + icon);
-            if (!FileUtils.exists(icon)) {
-                //LogManager.log("... icon file does not exist: " + icon);
-                InputStream is = null;
-                is = ResourceUtils.getResource(ICON_UNIX_RESOURCE, this.getClass().getClassLoader());
-                if (is != null) {
-                    FileOutputStream fos = null;
-                    try {
-                        fos = new FileOutputStream(icon);
-                        StreamUtils.transferData(is, fos);
-                        is.close();
-                        fos.close();
-
-                    } catch (IOException e) {
-
-                    } finally {
-                        if (fos != null) {
-                            try {
-                                fos.close();
-                            } catch (IOException e) {
-                            }
-                        }
-                    }
-                }
-            }
+        File iconLocation = new File(workingDirectory, icon);
+        if (!iconLocation.exists()) {
+            //TODO log warning
         }
 
-        if (SystemUtils.isWindows()) {
-            executable = new File(workingDirectory, executableName);
-        } else {
-            executable = new File(workingDirectory, executableName);
-        }
-        final String name = names.get(new Locale(StringUtils.EMPTY_STRING));
-        final FileShortcut shortcut = new FileShortcut(name, executable);
+        File executable = new File(workingDirectory, executableName);
+
+        final FileShortcut shortcut = new FileShortcut(productName, executable);
+        Map<Locale, String> names = new Hashtable<Locale, String>();
+        names.put(Locale.getDefault(), productName);
         shortcut.setNames(names);
+
+        Map<Locale, String> descriptions = new Hashtable<Locale, String>();
+        descriptions.put(Locale.getDefault(), productDescription);
         shortcut.setDescriptions(descriptions);
         shortcut.setCategories(SHORTCUT_CATEGORIES);
-        shortcut.setFileName(SHORTCUT_FILENAME);
-        shortcut.setIcon(icon);
-        shortcut.setRelativePath(relativePath);
+        shortcut.setFileName(productName);
+        shortcut.setIcon(iconLocation);
+        shortcut.setRelativePath(relativePath); //of the shortcut i.e. start menu/Organization/Application link
         shortcut.setWorkingDirectory(workingDirectory);
         shortcut.setModifyPath(true);
 
